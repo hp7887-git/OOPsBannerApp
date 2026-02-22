aac17c85a57649ec197bdef75294bf24a69fe47c
hp7887-git <hp7887@srmist.edu.in>
2026-02-22 21:13:46 +0530

    Printin OOPS using +

--- OOPsBanApp.java
+++ OOPsBanApp.java
@@ -1,5 +1,12 @@
 public class OOPsBanApp{
     public static void main(String[] args) {
-    System.err.println("OOPS");
-}
+
+        System.out.println("   ***   " + "    ***   " + "  ******" + "  ****** ");
+        System.out.println(" *     *  " + " *     *  " + " *     *" + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *     *" + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " ******" + "  ****** ");
+        System.out.println(" *     *  " + " *     *  " + " *         " + "   *  ");
+        System.out.println(" *     *  " + " *     *  " + " *         " + "   *  ");
+        System.out.println("   ***   " + "    ***   " + "  *       " + "****** ");
+    }
 }
